5d4e56784ec21323772064bb5940d11a91c992ce
Candice-Leigh-King <candyschofie@gmail.com>
2021-08-23 17:13:28 +0200

    update

--- main.js
+++ main.js
@@ -116,45 +116,7 @@ $(document).ready(function() {
 						 });
 					 });
 			 });
-	// This is the pop-up gallery
-	$('.popup-gallery').magnificPopup({
-		delegate: 'a',
-		type: 'image',
-		tLoading: 'Loading image #%curr%...',
-		mainClass: 'mfp-img-mobile',
-		gallery: {
-			enabled: true,
-			navigateByImgClick: true,
-			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
-		},
-		image: {
-			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-			titleSrc: function(item) {
-				return item.el.attr('title');
-			}
-		}
-	});
 
-
-		//make sure the page is ready befor running the scripts
-		// This is the hamburger menu
-
-
-// 	var allPanels = $('.accordion > dd').hide();
-//
-// 		$('.accordion > dt > a').click(function() {
-//
-// 			allPanels.slideUp();
-//
-// 	    $(this).filter('.open').removeClass('open').addClass('closing')
-// 	    $('.accordion > dt > a').removeClass('open');
-// 	  $(this).not(".closing").addClass('open').parent().next().slideDown();
-// 	    $('.accordion > dt > a').removeClass('closing');
-//
-// 	    return false;
-// 		});
-//
-//
  });
 
 // end accordian
